77cf127910be82511e70b5153a515271edb4fce9
ecole96 <ecole96@outlook.com>
2020-04-20 19:43:04 -0400

    similarity updates & performance tweaks

--- webapp/buildQuery.php
+++ webapp/buildQuery.php
@@ -1,50 +1,71 @@
 <?php
 	function buildQuery($connect,$search_query,$dateFrom,$dateTo,$source_search,$ID_search,$sourcebox,$mode) {
-        // preventing SQL injections...(sourcebox strings handled farther down)
+        // preventing SQL injections...(more complex strings handled farther down)
         $search_query = mysqli_real_escape_string($connect,$search_query);
         $dateFrom = mysqli_real_escape_string($connect,$dateFrom);
         $dateTo = mysqli_real_escape_string($connect,$dateTo);
         
         if($mode == 'download') {
-            /*$sql = "SELECT a.idArticle,a.n,a.datetime,a.date,a.url,a.source,a.author,a.title,a.article_text,a.score,a.magnitude,
-                    GROUP_CONCAT(DISTINCT keyword) as keywords, entity as top_entity, MAX(entity_instances.score) as top_entity_score, allsides_bias,allsides_confidence,allsides_agree,allsides_disagree,mbfs_bias,mbfs_score,factual_reporting
-                    FROM (SELECT idArticle,@n:=CASE WHEN @pubdate = date(datetime) THEN @n + 1 ELSE 1 END AS n, @pubdate:=date(datetime) as date,datetime,url,source,author,title,article_text,score,magnitude FROM article ORDER BY date, idArticle) a
-                    NATURAL JOIN (article_keywords NATURAL JOIN keyword_instances)
-                    LEFT JOIN (image NATURAL JOIN image_entities NATURAL JOIN entity_instances) ON a.idArticle = image.idArticle
+            $sql = "SELECT a.idArticle, CONCAT(a.date, '_', LPAD(a.n, 3, '0')) as alt_id, a.datetime, a.source, IFNULL(sb.mbfs_bias,''), IFNULL(sb.mbfs_score,''), IFNULL(sb.factual_reporting,''), IFNULL(sb.allsides_bias,''), IFNULL(sb.allsides_confidence,''), 
+                    IFNULL(sb.allsides_agree,''), IFNULL(sb.allsides_disagree,''), a.url, a.title, a.author, IFNULL(a.relevancy_score,''), IFNULL(a.score,''), IFNULL(a.magnitude,''), IFNULL(i.top_entity,''), IFNULL(i.top_entity_score,''), k.keywords, 
+                    IFNULL(sa.similarBefore,''), IFNULL(sa.similarAfter,''), IFNULL(a.fb_reactions_initial,''), IFNULL(a.fb_reactions_d1,''), IFNULL(a.fb_reactions_d7,''), IFNULL(a.fb_comments_initial,''), IFNULL(a.fb_comments_d1,''), IFNULL(a.fb_comments_d7,''), IFNULL(a.fb_shares_initial,''), 
+                    IFNULL(a.fb_shares_d1,''), IFNULL(a.fb_shares_d7,''), IFNULL(a.fb_comment_plugin_initial,''), IFNULL(a.fb_comment_plugin_d1,''), IFNULL(a.fb_comment_plugin_d7,''), IFNULL(a.tw_tweets_initial,''), IFNULL(a.tw_tweets_d1,''), IFNULL(a.tw_tweets_d7,''), 
+                    IFNULL(a.tw_favorites_initial,''), IFNULL(a.tw_favorites_d1,''), IFNULL(a.tw_favorites_d7,''), IFNULL(a.tw_retweets_initial,''), IFNULL(a.tw_retweets_d1,''), IFNULL(a.tw_retweets_d7,''), IFNULL(a.tw_top_favorites_initial,''), IFNULL(a.tw_top_favorites_d1,''), 
+                    IFNULL(a.tw_top_favorites_d7,''), IFNULL(a.tw_top_retweets_initial,''), IFNULL(a.tw_top_retweets_d1,''), IFNULL(a.tw_top_retweets_d7,''), IFNULL(a.rdt_posts_initial,''), IFNULL(a.rdt_posts_d1,''), IFNULL(a.rdt_posts_d7,''), IFNULL(a.rdt_total_comments_initial,''), 
+                    IFNULL(a.rdt_total_comments_d1,''), IFNULL(a.rdt_total_comments_d7,''), IFNULL(a.rdt_total_scores_initial,''), IFNULL(a.rdt_total_scores_d1,''), IFNULL(a.rdt_total_scores_d7,''), IFNULL(a.rdt_top_comments_initial,''), IFNULL(a.rdt_top_comments_d1,''), 
+                    IFNULL(a.rdt_top_comments_d7,''), IFNULL(a.rdt_top_score_initial,''), IFNULL(a.rdt_top_score_d1,''), IFNULL(a.rdt_top_score_d7,''), IFNULL(a.rdt_top_ratio_initial,''), IFNULL(a.rdt_top_ratio_d1,''), IFNULL(a.rdt_top_ratio_d7,''), IFNULL(a.rdt_avg_ratio_initial,''), 
+                    IFNULL(a.rdt_avg_ratio_d1,''), IFNULL(a.rdt_avg_ratio_d7,'') 
+                    FROM (SELECT @n:=CASE WHEN @pubdate = date(datetime) THEN @n + 1 ELSE 1 END AS n, @pubdate:=date(datetime) as date, idArticle, url, source, author, datetime, title, score, magnitude, relevancy_score,fb_reactions_initial, 
+                                    fb_comments_initial, fb_shares_initial, fb_comment_plugin_initial, tw_tweets_initial, tw_favorites_initial, tw_retweets_initial, tw_top_favorites_initial, tw_top_retweets_initial, rdt_posts_initial, rdt_total_comments_initial, 
+                                    rdt_total_scores_initial,rdt_top_comments_initial,rdt_top_score_initial, rdt_top_ratio_initial, rdt_avg_ratio_initial, fb_reactions_d1, fb_comments_d1, fb_shares_d1,fb_comment_plugin_d1, tw_tweets_d1, tw_favorites_d1, 
+                                    tw_retweets_d1, tw_top_favorites_d1, tw_top_retweets_d1, rdt_posts_d1, rdt_total_comments_d1,rdt_total_scores_d1, rdt_top_comments_d1, rdt_top_score_d1, rdt_top_ratio_d1, rdt_avg_ratio_d1, fb_reactions_d7, fb_comments_d7, 
+                                    fb_shares_d7,fb_comment_plugin_d7, tw_tweets_d7, tw_favorites_d7, tw_retweets_d7, tw_top_favorites_d7, tw_top_retweets_d7,rdt_posts_d7, rdt_total_comments_d7, rdt_total_scores_d7, rdt_top_comments_d7, rdt_top_score_d7, 
+                                    rdt_top_ratio_d7, rdt_avg_ratio_d7 
+                                FROM article ORDER BY date, idArticle) a
+                    NATURAL JOIN
+                        (SELECT idArticle, GROUP_CONCAT(keyword ORDER BY keyword ASC) as keywords from keyword_instances NATURAL JOIN article_keywords GROUP BY idArticle) k
+                    LEFT JOIN
+                        (SELECT ii.idArticle,ii.entity as top_entity,imax.top_entity_score
+                            FROM (SELECT idArticle,entity,score FROM image NATURAL JOIN entity_instances NATURAL JOIN image_entities) ii
+                            INNER JOIN
+                            (SELECT idArticle,entity,MAX(score) as top_entity_score FROM image NATURAL JOIN entity_instances NATURAL JOIN image_entities GROUP BY idArticle) imax
+                            ON ii.idArticle=imax.idArticle AND ii.entity=imax.entity AND ii.score=imax.top_entity_score) i 
+                        ON a.idArticle=i.idArticle
                     LEFT JOIN (
-                                (SELECT b1.source,allsides_bias,allsides_confidence,allsides_agree,allsides_disagree,mbfs_bias,mbfs_score,factual_reporting
-                                FROM source_bias b1
-                                INNER JOIN
-                                    (SELECT source,MIN(allsides_id) min_id
-                                    FROM source_bias
-                                    GROUP BY source) b2 ON b2.source=b1.source
-                                AND b1.allsides_id = b2.min_id)
-                            UNION
-                                (SELECT source,allsides_bias,allsides_confidence,allsides_agree,allsides_disagree,mbfs_bias,mbfs_score,factual_reporting
-                                FROM source_bias
-                                WHERE allsides_bias IS NULL
-                                    AND mbfs_bias IS NOT NULL)) bias ON a.source=bias.source ";*/
-            $sql = "SELECT a.*, GROUP_CONCAT(DISTINCT keyword) as keywords, entity as top_entity, MAX(entity_instances.score) as top_entity_score, 
-                    allsides_bias,allsides_confidence,allsides_agree,allsides_disagree,mbfs_bias,mbfs_score,factual_reporting
-                    FROM (SELECT *, @n:=CASE WHEN @pubdate = date(datetime) THEN @n + 1 ELSE 1 END AS n, @pubdate:=date(datetime) as date FROM article ORDER BY date, idArticle) a
-                    NATURAL JOIN (article_keywords NATURAL JOIN keyword_instances)
-                    LEFT JOIN (image NATURAL JOIN image_entities NATURAL JOIN entity_instances) ON a.idArticle = image.idArticle
-                    LEFT JOIN (
-                                (SELECT b1.source,allsides_bias,allsides_confidence,allsides_agree,allsides_disagree,mbfs_bias,mbfs_score,factual_reporting
-                                FROM source_bias b1
-                                INNER JOIN
-                                    (SELECT source,MIN(allsides_id) min_id
-                                    FROM source_bias
-                                    GROUP BY source) b2 ON b2.source=b1.source
-                                AND b1.allsides_id = b2.min_id)
-                            UNION
-                                (SELECT source,allsides_bias,allsides_confidence,allsides_agree,allsides_disagree,mbfs_bias,mbfs_score,factual_reporting
-                                FROM source_bias
-                                WHERE allsides_bias IS NULL
-                                    AND mbfs_bias IS NOT NULL)) bias ON a.source=bias.source ";
+                        (SELECT b1.source,allsides_bias,allsides_confidence,allsides_agree,allsides_disagree,mbfs_bias,mbfs_score,factual_reporting
+                        FROM source_bias b1
+                        INNER JOIN
+                            (SELECT source,MIN(allsides_id) min_id
+                            FROM source_bias
+                            GROUP BY source) b2 
+                            ON b2.source=b1.source AND b1.allsides_id = b2.min_id)
+                        UNION
+                        (SELECT source,allsides_bias,allsides_confidence,allsides_agree,allsides_disagree,mbfs_bias,mbfs_score,factual_reporting
+                        FROM source_bias
+                        WHERE allsides_bias IS NULL AND mbfs_bias IS NOT NULL)) sb 
+                        ON a.source=sb.source
+                    LEFT JOIN
+                        (
+                            SELECT sa_raw.idArticle, GROUP_CONCAT(CASE WHEN idArticle_datetime >= otherArticle_datetime THEN CONCAT(otherArticle,':',similarity) ELSE null END ORDER BY similarity DESC, otherArticle DESC) as similarBefore,
+                            GROUP_CONCAT(CASE WHEN idArticle_datetime < otherArticle_datetime THEN CONCAT(otherArticle,':',similarity) ELSE null END ORDER BY similarity DESC, otherArticle DESC) as similarAfter
+                            FROM 
+                            (SELECT article1 as idArticle,article2 as otherArticle,similarity FROM similar_articles
+                            UNION ALL
+                            SELECT article2 as idArticle,article1 as otherArticle,similarity FROM similar_articles) sa_raw
+                            INNER JOIN
+                            (SELECT idArticle, datetime as idArticle_datetime FROM article) ia
+                            ON ia.idArticle=sa_raw.idArticle
+                            INNER JOIN
+                            (SELECT idArticle, datetime as otherArticle_datetime FROM article) oa
+                            ON oa.idArticle=sa_raw.otherArticle
+                            GROUP BY sa_raw.idArticle
+                        ) sa 
+                        ON a.idArticle=sa.idArticle ";
         }
         else {
-            $sql = "SELECT idArticle,source,title,date(datetime) as date,GROUP_CONCAT(keyword) as keywords FROM article a NATURAL JOIN (article_keywords NATURAL JOIN keyword_instances) ";
+            $sql = "SELECT a.idArticle,source,title,date(datetime) as date ";
+            if(!empty($search_query)) { $sql .= ", keywords "; }
+            $sql .= "FROM article a ";
             if($mode == 'sourcebox') {
                 $sql = "SELECT source, count(source) FROM (" . $sql;
             }
@@ -52,12 +73,24 @@
 
         $conditionsExist = false; // boolean to determine whether WHERE or AND is used in query statement (if true, initial condition has already been set so subsequent conditions are prefixed with AND)
 
+        // primary search box (text, checks title and keywords)
+        if(!empty($search_query)) {
+            if($mode != 'download') {  // unless we're downloading or doing a text search, we don't need to use the extra resources to gather keywords
+                $sql .= 'NATURAL JOIN 
+                            (SELECT idArticle, GROUP_CONCAT(keyword) as keywords FROM keyword_instances NATURAL JOIN article_keywords GROUP BY idArticle) k ';
+            };
+            $search_str = "WHERE (title LIKE '%$search_query%' OR keywords LIKE '%$search_query%') ";
+            $conditionsExist = true;
+            $sql .= $search_str;
+        }
+
         // date range search - if no dates provided, ignore
         if(!empty($dateFrom) && !empty($dateTo)) {
             // convert date input to Y-m-d format - this is because the bootstrap datepicker sends dates in Y/m/d while SQL only accepts as Y-m-d
         	$dateFrom = date("Y-m-d",strtotime($dateFrom));
             $dateTo = date("Y-m-d",strtotime($dateTo));
-            $date_str = "WHERE date(datetime) BETWEEN '$dateFrom' AND '$dateTo' ";
+            $date_str = !$conditionsExist ? "WHERE " : "AND ";
+            $date_str .= "(date(datetime) BETWEEN '$dateFrom' AND '$dateTo') ";
             $conditionsExist = true;
             $sql .= $date_str;
         }
@@ -66,21 +99,21 @@
         if(!empty($sourcebox) && $mode != 'sourcebox') {
             $source_filter_str = !$conditionsExist ? "WHERE " : "AND ";
             $conditionsExist = true;
-            $source_filter_str .= "a.source IN "; 
+            $source_filter_str .= "(a.source IN "; 
             $sourcebox_safe = array(); // build an array of sources in SQL string format, ex: ['source1','source2','source3'], all escaped to prevent SQL injections
             foreach($sourcebox as $source) {
                 $source_str = "'" . mysqli_real_escape_string($connect,$source) . "'";
                 array_push($sourcebox_safe,$source_str);
             }
             $source_filter_str .= "(" . implode(",",$sourcebox_safe) . ") "; // implode array by comma delimiter, bookended by parentheses to give us full list of sources to query against in SQL format
-            $sql .= $source_filter_str;
+            $sql .= $source_filter_str . ')';
         }
 
         // source search box (different from sourcebox, though functionally the same)
         if(!empty($source_search)) {
             $ss_str = !$conditionsExist ? "WHERE " : "AND ";
             $conditionsExist = true;
-            $ss_str .= "a.source IN ";
+            $ss_str .= "(a.source IN ";
             $ss_arr = preg_split('/\s+/', $source_search, -1, PREG_SPLIT_NO_EMPTY); // delimit by whitespace
             $ss_safe = array();
             foreach($ss_arr as $s) {
@@ -88,13 +121,13 @@
                 array_push($ss_safe,$s_str);
             }
             $ss_str .= "(" . implode(",",$ss_safe) . ") ";
-            $sql .= $ss_str;
+            $sql .= $ss_str . ')';
         }
 
         if(!empty($ID_search)) {
             $id_str = !$conditionsExist ? "WHERE " : "AND ";
             $conditionsExist = true;
-            $id_str .= "a.idArticle IN ";
+            $id_str .= "(a.idArticle IN ";
             $id_arr = preg_split('/\s+/', $ID_search, -1, PREG_SPLIT_NO_EMPTY);
             $id_safe = array();
             foreach($id_arr as $i) {
@@ -102,16 +135,11 @@
                 array_push($id_safe,$i_str);
             }
             $id_str .= "(" . implode(",",$id_safe) . ") ";
-            $sql .= $id_str;
+            $sql .= $id_str . ')';
         }
-
-        $sql .= "GROUP BY a.idArticle ";
-        if(!empty($search_query)) { // HAVING clause necessary to correctly check search query against list of keywords 
-            $sql .= "HAVING title LIKE '%$search_query%' OR keywords LIKE '%$search_query%' ";
-        }
-        if($mode == "download") { $sql .= "ORDER BY a.idArticle DESC"; }
+        if($mode == "download") { $sql .= "ORDER BY a.idArticle DESC "; }
         else if($mode == 'sourcebox') { $sql .= ") AS results GROUP BY source ORDER BY source"; }
-        //echo $sql;
+
         return $sql;
     }
 ?>
--- webapp/download.php
+++ webapp/download.php
@@ -1,12 +1,11 @@
 <?php
     // does the same SQL search query as the one in search.php for any given search, but outputs the rows to a .csv + article text in .txt
     // everything is stored inside a .zip file
-
     include_once("authenticate.php");
     include_once("db_connect.php");
     include("buildQuery.php");
 
-    ini_set('memory_limit','1G'); // large downloads were hitting some memory usage limit once keywords were added to the CSV - upped it here (will likely need to increase with size of the database)
+    ini_set('memory_limit','250M'); // large downloads were hitting some memory usage limit once keywords were added to the CSV - upped it here (will likely need to increase with size of the database)
     ignore_user_abort(true); // still delete temp files if user cancels download
     set_time_limit(300);
 
@@ -17,53 +16,59 @@
     $ID_search = (!empty($_GET['ID_search']) ? trim($_GET['ID_search']) : '');
     $sourcebox = (!empty($_GET['sourcebox']) ? $_GET['sourcebox'] : '');
 
+    // Download article data into a .zip file consisting of a single .csv file with all of the search results + individual .txt files for each article's content
+    $download_id = uniqid(); // download identifier used in zip and csv filenames to differentiate one download instance from another (in case of simultaneous downloads from multiple users)
+
+    $curdir = getcwd();
+    $csvName = "article_$download_id.csv";
+
     $sql = buildQuery($connect,$search_query,$dateFrom,$dateTo,$source_search,$ID_search,$sourcebox,"download");
+    $outfile_sql = "INTO OUTFILE '$curdir/$csvName'
+                    FIELDS TERMINATED BY '\\t' 
+                    OPTIONALLY ENCLOSED BY '\\\"' ESCAPED BY '\"'
+                    LINES TERMINATED BY '\\n'"; // necessary for MySQL to directly generate the CSV
+    $sql .= $outfile_sql;
+
     // settings user variables necessary for calculating an article's Alt ID
+    mysqli_query($connect,"SET SESSION group_concat_max_len = 1000000"); // need to temporarily increase GROUP_CONCAT() length
     mysqli_query($connect,"SET @n=0");
     mysqli_query($connect,"SET @pubdate=''");
     $query = mysqli_query($connect, $sql) or die(mysqli_connect_error()); // execute query
+    mysqli_close($connect);
 
-    // Download article data into a .zip file consisting of a single .csv file with all of the search results + individual .txt files for each article's content
-    $download_id = uniqid(); // download identifier used in zip and csv filenames to differentiate one download instance from another (in case of simultaneous downloads from multiple users)
-    $zipName = "articles_" . $download_id . ".zip";
+    $zipName = "articles_$download_id.zip";
     $zip = new ZipArchive(); // create a zip file
-    if ($zip->open($zipName, ZipArchive::CREATE) && $query)
+    if ($zip->open($zipName, ZipArchive::CREATE) && $query && file_exists($csvName))
     {
-        $csvName = "article_data_" . $download_id . ".csv";
-        $csv = fopen($csvName, 'w') or die ("Unable to generate CSV: " . $csvName);
-
-        // CSV column headers
-        $headers = array("Article ID", "Alt ID", "Date/Time", "Source", "MBFS Bias","MBFS Score","MBFS Factual Reporting", "AllSides Bias","AllSides Confidence","AllSides Agreement","AllSides Disagreement","URL","Title","Author","Relevancy Score","Sentiment Score","Sentiment Magnitude","Top Image Entity","Entity Score","Keywords");
-        $metric_headers = array("FB Reactions"=>"fb_reactions","FB Comments"=>"fb_comments","FB Shares"=>"fb_shares","FB Comment Plugin"=>"fb_comment_plugin", 
-                         "TW Tweets"=>"tw_tweets","TW Total Favorites"=>"tw_favorites","TW Total Retweets"=>"tw_retweets","TW Top Favorites"=>"tw_top_favorites","TW Top Retweets"=>"tw_top_retweets",
-                         "RDT Posts"=>"rdt_posts","RDT Total Comments"=>"rdt_total_comments","RDT Total Scores"=>"rdt_total_scores","RDT Top Comments"=>"rdt_top_comments",
-                         "RDT Top Score"=>"rdt_top_score","RDT Top Ratio"=>"rdt_top_ratio","RDT Average Ratio"=>"rdt_avg_ratio");
-        foreach($metric_headers as $header=>$colname) { // dynamically adding social media metric headers
-            array_push($headers,"$header - Initial Entry"); array_push($headers,"$header - Day 1"); array_push($headers,"$header - Day 7"); 
-        }
-        fputcsv($csv, $headers,"\t");
-        // build files to go into zip
-        $txt_path = "../txtfiles/"; // where all txt files are stored
-        while ($row = mysqli_fetch_assoc($query)) {
-            $altID = $row['date'] . '_' . sprintf("%03d",$row['n']); // alternate ID as requested in Y-m-d_n format, where n is the Nth article of its publishing date (sorted by idArticle ascending) [n is 3 digits, with leading zeroes as necessary]
-            $data = array($row['idArticle'],$altID,$row['datetime'], $row['source'],$row['mbfs_bias'],$row['mbfs_score'],$row['factual_reporting'],$row['allsides_bias'],$row['allsides_confidence'],$row['allsides_agree'],
-                        $row['allsides_disagree'],  $row['url'], $row['title'], $row['author'], $row['relevancy_score'],$row['score'],$row['magnitude'],
-                        $row['top_entity'],$row['top_entity_score'],$row['keywords']);
-            foreach($metric_headers as $header=>$colname) { // dynamically adding social media metric data
-                array_push($data,$row["$colname" . "_initial"]); array_push($data,$row["$colname" . "_d1"]); array_push($data,$row["$colname" . "_d7"]); 
+        if (($f = fopen($csvName, "r")) !== FALSE) { // open CSV and get the article ID of each row to decide which text files need to be added to the archive
+            $txtdir = dirname(__DIR__) . '/txtfiles/';
+            while (($row = fgetcsv($f,0,"\t","\"",'"')) !== FALSE) {
+                $txtfile = $row[0] . ".txt";
+                if(file_exists($txtdir . $txtfile)) {
+                    $zip->addFile($txtdir . $txtfile,$txtfile);
+                }
             }
-            fputcsv($csv, $data,"\t"); // insert row in CSV (tab delimiter necessary for Excel compatibility fix)
-            $txtName = $row['idArticle'] . ".txt"; // get {idArticle}.txt file from /txtfiles/ folder
-            if (file_exists($txt_path . $txtName)) {
-                $zip->addFile($txt_path . $txtName, $txtName); // add .txt to zip
-            }
+            fclose($f);
         }
-        fclose($csv); // CSV finished - all rows inserted
 
+        $headers = array('"Article ID"', '"Alt ID"', '"Date/Time"', '"Source"', '"MBFS Bias"', '"MBFS Score"', '"MBFS Factual Reporting"', '"AllSides Bias"', '"AllSides Confidence"', 
+                        '"AllSides Agreement"', '"AllSides Disagreement"', '"URL"', '"Title"', '"Author"', '"Relevancy Score"', '"Sentiment Score"', '"Sentiment Magnitude"', '"Top Image Entity"', 
+                        '"Entity Score"', '"Keywords"', '"Similar Articles - Before Publication"', '"Similar Articles - After Publication"','"FB Reactions - Initial Entry"', '"FB Reactions - Day 1"', '"FB Reactions - Day 7"', '"FB Comments - Initial Entry"', 
+                        '"FB Comments - Day 1"', '"FB Comments - Day 7"', '"FB Shares - Initial Entry"', '"FB Shares - Day 1"', '"FB Shares - Day 7"', '"FB Comment Plugin - Initial Entry"', 
+                        '"FB Comment Plugin - Day 1"', '"FB Comment Plugin - Day 7"', '"TW Tweets - Initial Entry"', '"TW Tweets - Day 1"', '"TW Tweets - Day 7"', '"TW Total Favorites - Initial Entry"', 
+                        '"TW Total Favorites - Day 1"', '"TW Total Favorites - Day 7"', '"TW Total Retweets - Initial Entry"', '"TW Total Retweets - Day 1"', '"TW Total Retweets - Day 7"', 
+                        '"TW Top Favorites - Initial Entry"', '"TW Top Favorites - Day 1"', '"TW Top Favorites - Day 7"', '"TW Top Retweets - Initial Entry"', '"TW Top Retweets - Day 1"', 
+                        '"TW Top Retweets - Day 7"', '"RDT Posts - Initial Entry"', '"RDT Posts - Day 1"', '"RDT Posts - Day 7"', '"RDT Total Comments - Initial Entry"', '"RDT Total Comments - Day 1"', 
+                        '"RDT Total Comments - Day 7"', '"RDT Total Scores - Initial Entry"', '"RDT Total Scores - Day 1"', '"RDT Total Scores - Day 7"', '"RDT Top Comments - Initial Entry"', 
+                        '"RDT Top Comments - Day 1"', '"RDT Top Comments - Day 7"', '"RDT Top Score - Initial Entry"', '"RDT Top Score - Day 1"', '"RDT Top Score - Day 7"', '"RDT Top Ratio - Initial Entry"', 
+                        '"RDT Top Ratio - Day 1"', '"RDT Top Ratio - Day 7"', '"RDT Average Ratio - Initial Entry"', '"RDT Average Ratio - Day 1"', '"RDT Average Ratio - Day 7"');
+
+        // append headers to CSV file (because MySQL's INTO OUTFILE command doesn't include them)
+        $data = implode("\t",$headers) . "\n" . file_get_contents($csvName); 
         // the CSV is by default in UTF-8 encoding, which causes some scrambled characters in Excel (like apostrophes), so we convert it to UTF-16LE to fix this
-        $data = file_get_contents($csvName); 
-        $data = chr(255) . chr(254) . mb_convert_encoding($data, 'UTF-16LE','UTF-8');
+        $data = chr(255) . chr(254) . mb_convert_encoding($data, 'UTF-16LE','UTF-8'); 
         file_put_contents($csvName, $data);
+        unset($data);
 
         $zip->addFile($csvName,$csvName); // add completed CSV to zip
         $zip->close(); // finish zip
@@ -76,11 +81,11 @@
         header("Content-Length: " . filesize($zipName));
 
         unlink($csvName); // delete csv (no longer needed now that it's in the zip archive)
-        readfile($zipName); // download zip
+        readfile($zipName); // serve .zip to user
         unlink($zipName); // delete zip from server
     }
     else
     {
-        echo "ERROR: Couldn't download file!";
+        echo "ERROR: Couldn't download files!";
     }
 ?>
