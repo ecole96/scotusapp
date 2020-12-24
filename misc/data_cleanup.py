6f2f039c2e826a6853032e2624c5062da52ba12e
ecole96 <ecole96@outlook.com>
2020-12-24 11:47:39 -0500

    misc files

--- misc/data_cleanup.py
+++ misc/data_cleanup.py
@@ -1,5 +1,4 @@
 # this script goes through our training data spreadsheet and moves false positives into the rejectedTrainingData table of our database, rather than the primary front-facing one
-
 import csv
 import MySQLdb
 import MySQLdb.cursors
@@ -7,7 +6,9 @@ import os
 from tqdm import tqdm
 import sys
 
-def move_data(c,filename):
+# wrapper function for performing cleanup tasks
+# reads cleanup CSV row by row, grabs article data, deletes the article's images, transfers the data to the rejectedTrainingData table, and then removes the article (and any leftover data) from the primary database
+def cleanup(c,filename,img_dir):
     with open(filename) as csvfile: # change filename appropriately here
         reader = csv.DictReader(csvfile)
         total = len(list(reader))
@@ -15,34 +16,45 @@ def move_data(c,filename):
         reader = csv.DictReader(csvfile)
         for row in tqdm(iterable=reader,desc='Training Data',total=total):
             try:
-                if row['Code'] != 'R':
+                if row['Code'] != 'R': # only moving irrelevant articles
                     ID = row['Article ID'].strip()
-                    c.execute("""SELECT url, title, datetime, article_text, GROUP_CONCAT(DISTINCT keyword) as keywords 
-                        FROM article NATURAL JOIN article_keywords NATURAL JOIN keyword_instances 
-                        WHERE idArticle=%s GROUP BY idArticle LIMIT 1""",(ID,))
+                    c.execute("""SELECT a.url, title, datetime, article_text, GROUP_CONCAT(DISTINCT keyword) as keywords, GROUP_CONCAT(DISTINCT path) as images 
+                                FROM article A NATURAL JOIN article_keywords NATURAL JOIN keyword_instances LEFT JOIN image I ON A.idArticle = I.idArticle 
+                                WHERE A.idArticle=%s LIMIT 1""",ID)
                     if c.rowcount == 1:
                         article = c.fetchone()
+                        if article['images'] is not None:
+                            delete_images(img_dir,article['images'])
                         t = (article['url'],article['datetime'],article['title'],article['article_text'],article['keywords'],row['Code'].strip())
                         c.execute("""INSERT INTO rejectedTrainingData(url,datetime,title,text,keywords,code) VALUES (%s,%s,%s,%s,%s,%s)""",t)
                         c.execute("""DELETE FROM article WHERE idArticle=%s LIMIT 1""",(ID,))
             except Exception as e:
                 print(e)
                 continue
+        delete_extra_db(c)
 
-def delete_images(c):
-    x=3
+# delete images associated with an irrelevant article
+def delete_images(img_dir,img_str_list):
+    images = img_str_list.split(",")
+    for i in images:
+        abs_image_path = img_dir + i
+        if os.path.exists(abs_image_path):
+            os.remove(abs_image_path)
 
-def delete_extra_db(c):
+# deleting articles often results in keywords and entities that don't belong to any articles and just take up space, so deleting them here
+def delete_extra_db(c): 
     c.execute("""DELETE FROM article_keywords WHERE idKey NOT IN (SELECT DISTINCT idKey FROM keyword_instances)""")
     c.execute("""DELETE FROM image_entities WHERE idEntity NOT IN (SELECT DISTINCT idEntity FROM entity_instances)""")
 
 def main():
-    if len(sys.argv) < 2:
-        print("Needs CSV argument.")
+    if len(sys.argv) < 3:
+        print("Needs CSV and image folder argument (in that order.")
     else:
         filename = sys.argv[1]
+        img_dir = sys.argv[2]
+        if img_dir[-1] != "/": img_dir += "/" # accounting for trailing slash
         db = MySQLdb.connect(host=os.environ['DB_HOST'],port=int(os.environ['DB_PORT']),user=os.environ['DB_USER'],password=os.environ['DB_PASSWORD'],db="SupremeCourtApp",use_unicode=True,charset="utf8")
         db.autocommit(True)
         c = db.cursor(MySQLdb.cursors.DictCursor)
-        move_data(c,filename)
+        cleanup(c,filename,img_dir)
 main()
--- misc/test_classifier.py
+++ misc/test_classifier.py
@@ -12,11 +12,6 @@ import MySQLdb.cursors
 import os
 import numpy as np
 import tldextract
-import re
-import string
-import nltk
-from nltk.stem import WordNetLemmatizer
-from nltk.corpus import wordnet
 
 def getSource(url):
     ext = tldextract.extract(url)
@@ -33,7 +28,7 @@ def get_training_data(c,binary):
         x.append([r["title"],r["article_text"],r["keywords"],r["url"],r["source"]])
         y.append("R")
     # get irrelevant training data 13276
-    c.execute("""SELECT * FROM rejectedTrainingData""")
+    c.execute("""SELECT * FROM rejectedTrainingData WHERE id <= 26301""")
     rows = c.fetchall()
     for r in rows:
         x.append([r["title"],r["text"],r["keywords"],r["url"],getSource(r["url"])])
@@ -43,77 +38,31 @@ def get_training_data(c,binary):
         y.append(code)
     return x, y
 
-def convertTextData(x,v_text,v_title,v_keys,mode):
+def convertTextData(x,v_text,v_title,mode):
     Xtitle = []
     Xtext = []
-    #Xkeys = []
     for row in x:
         Xtitle.append(row[0])
         Xtext.append(row[1])
-        #Xkeys.append(row[2])
     if mode == 'train':
         Xtitle = v_title.fit_transform(Xtitle)
         Xtext = v_text.fit_transform(Xtext)
-        #Xkeys = v_keys.fit_transform(Xkeys)
     else:
         Xtitle = v_title.transform(Xtitle)
         Xtext = v_text.transform(Xtext)
-        #Xkeys = v_keys.transform(Xkeys)
-    #x = hstack([Xtext,Xtitle,Xkeys])
     x = hstack([Xtext,Xtitle])
     return x
-
-def preprocess(s):
-    s = s.lower()
-
 
 def prepare_data(training_texts,training_labels):
     Xtrain, Xtest_raw, Ytrain, Ytest = train_test_split(training_texts,training_labels,test_size=0.25)
     Xtrain = [x[:2] for x in Xtrain]
-    #s.lower().translate(str.maketrans('', '', string.punctuation))
     stop_words = stopwords.words("english")
     v_text = TfidfVectorizer(stop_words=stop_words,min_df=5)
     v_title = TfidfVectorizer(stop_words=stop_words,ngram_range=(1,3))
-    #v_keys = TfidfVectorizer(stop_words=[w.replace("'","") for w in stop_words],min_df=25,binary=True)
-    #v_text = TfidfVectorizer(tokenizer=LemmaTokenizer(),min_df=5)
-    #v_title = TfidfVectorizer(tokenizer=LemmaTokenizer(),ngram_range=(1,3))
-    v_keys = None
-    Xtrain = convertTextData(Xtrain,v_text,v_title,v_keys,'train')
+    Xtrain = convertTextData(Xtrain,v_text,v_title,'train')
     Xtest = [x[:2] for x in Xtest_raw]
-    Xtest = convertTextData(Xtest,v_text,v_title,v_keys,'test')
+    Xtest = convertTextData(Xtest,v_text,v_title,'test')
     return Xtrain, Xtest, Ytrain, Ytest, Xtest_raw
-
-class LemmaTokenizer(object):
-    def __init__(self):
-        self.wnl = WordNetLemmatizer()
-        self.stop_words = stopwords.words("english") + ["'s","n't"]
-    def __call__(self, doc):
-        #doc = doc.lower()
-        chars = {"’":"'","‘":"'","\xa0":" ","“":'"',"”":'"'}
-        for c in chars: doc = doc.replace(c,chars[c])
-        tokens = []
-        sents = nltk.sent_tokenize(doc)
-        for sent in sents:
-            sent_words = nltk.pos_tag(nltk.word_tokenize(sent))
-            for sw in sent_words:
-                if sw[1][0].isalpha():
-                    word = sw[0]
-                    if word not in self.stop_words and not word.startswith("http") and not word.startswith("//"):
-                        word = re.sub(r'([^\w]|_)+', '', word)
-                        if len(word) > 0:
-                            tag = get_wordnet_pos(sw[1])
-                            lemmatized = self.wnl.lemmatize(word,tag) 
-                            tokens.append(lemmatized)
-        return tokens
-
-def get_wordnet_pos(tag_full):
-    tag = tag_full[0].upper()
-    """Map POS tag to first character lemmatize() accepts"""
-    tag_dict = {"J": wordnet.ADJ,
-                "N": wordnet.NOUN,
-                "V": wordnet.VERB,
-                "R": wordnet.ADV}
-    return tag_dict.get(tag, wordnet.NOUN)
 
 def test_raw(c,n,binary):
     header = "Test: Classifier Only, "
@@ -138,19 +87,12 @@ def test_practical(c,n,binary):
     modestr = "Binary Label (R / U), " if binary else "Multi-Label (R / U / S / F), "
     header += modestr + str(n) + " Runs"
     print(header)
-    instantTerms = ["usa supreme court", "us supreme court", "u.s. supreme court", "united states supreme court", "scotus",
-                    'john roberts', 'anthony kennedy', 'clarence thomas', 'ruth bader ginsburg', 'stephen breyer', 
-                    'samuel alito', 'sonia sotomayor', 'elena kagan', 'neil gorsuch', 'brett kavanaugh', "antonin scalia"]
-    justice_keys = ['roberts', 'kennedy', 'thomas', 'ginsburg', 'breyer', 'alito', 
-                    'sotomayor', 'kagan', 'gorsuch', 'kavanaugh','scalia']
-    instantSources = ["scotusblog"]
     scores = []
     x, y = get_training_data(c,binary)
     for run in range(n):
         Xtrain, Xtest, Ytrain, Ytest, Xtest_raw = prepare_data(x, y)
         clf = CalibratedClassifierCV(LinearSVC(class_weight='balanced'),method='sigmoid',cv=5).fit(Xtrain,Ytrain)
         predict_probs = clf.predict_proba(Xtest)
-        Y_initial = []
         Y_process = []
         i = 0
         for p in predict_probs:
@@ -159,29 +101,10 @@ def test_practical(c,n,binary):
             source = Xtest_raw[i][4]
             url = Xtest_raw[i][3]
             text = Xtest_raw[i][1]
-            prob_by_class = dict(zip(clf.classes_,p)) # zip probabilities to their according class
-            sorted_probs = sorted(prob_by_class.items(),key=lambda kv: kv[1],reverse=True) # sort dict into an array of tuples most to least probable by class format: [(class,probability)]
-            result = sorted_probs[0][0] # initial / highest classification
-            Y_initial.append(result)
-            result_prob = sorted_probs[0][1] # highest probability
-            if any(term in title.lower() for term in (instantTerms + justice_keys)) or source in instantSources: 
-                result = "R"
-            elif stateCourtDetected(text,title):
-                result = "S"
-            elif foreignCourtDetected(source,text,title,url):
-                result = "F"
-            else:
-                if result == 'R':
-                    if not (('supreme' in keywords and 'court' in keywords) or 'scotus' in keywords) and not any(justice in keywords for justice in justice_keys): 
-                        relevancy_threshold = 0.75 # numbers subject to change (still trying to find a "sweet spot" but this seems to do well)
-                    else:
-                        relevancy_threshold = 0.5
-                    if result_prob < relevancy_threshold:
-                            result = sorted_probs[1][0]
-                            result_prob = sorted_probs[1][1]
-            if binary and result in ['F','S']:
-                result = "U"
-            Y_process.append(result)
+            code, class_score = relevancy_check(clf,p,text,title,source,url,keywords)
+            if binary and code in ['F','S']:
+                code = "U"
+            Y_process.append(code)
             i += 1
         accuracy = accuracy_score(Ytest,Y_process) * 100
         print("Run",str(run+1) + ": ",str(accuracy)+"%")
@@ -189,13 +112,67 @@ def test_practical(c,n,binary):
     avg = (sum(scores) / len(scores))
     print("Average:",str(avg)+"%\n")
     return avg
+            
+# relevancy check function - True for relevant, False otherwise
+def relevancy_check(clf,probabilities,text,title,source,url,keywords):
+    instantTerms = ["usa supreme court", "us supreme court", "u.s. supreme court", "united states supreme court", "scotus",
+                    'john roberts', 'anthony kennedy', 'clarence thomas', 'ruth bader ginsburg', 'stephen breyer', 
+                    'samuel alito', 'sonia sotomayor', 'elena kagan', 'neil gorsuch', 'brett kavanaugh', "antonin scalia"] # dead giveaways for relevancy
+    justice_keys = ['roberts', 'kennedy', 'thomas', 'ginsburg', 'breyer', 'alito', 
+                    'sotomayor', 'kagan', 'gorsuch', 'kavanaugh','scalia'] # last names of the justices for parsing in keywords
+    instantSources = ["scotusblog"]
+    # check for the "dead giveaways"
+    if any(term in title.lower() for term in (instantTerms + justice_keys)) or source in instantSources:
+        code = "R"
+        class_score = 1.0
+    elif stateCourtDetected(text,title):
+        code = "S"
+        class_score = 1.0
+    elif foreignCourtDetected(source,url,text,title):
+        code = "F"
+        class_score = 1.0
+    else:
+        code, class_score = classify(clf,probabilities,keywords,justice_keys)
+    return code, class_score
+
+# determine whether the focus of an article is about a state Supreme Court - returns True if deemed so, False otherwise
+# done by parsing the title and part of the text for giveaway terms ("[state] Supreme Court", "[state] high court", "[state] top court"...)
+def stateCourtDetected(text,title):
+    # dict in key:[] format where key is state and [] consists of common state abbreviations
+    states = {'alaska': ['ak'], 'alabama': ['al','ala'], 'arkansas': ['ar'], 'arizona': ['az','ariz'], 'california': ['ca','calif'], 'colorado': ['co'], 'connecticut': ['ct'], 'delaware': ['de'], 
+    'florida': ['fl','fla'], 'georgia': ['ga'], 'hawaii': ['hi'], 'iowa': ['ia'], 'idaho': ['id'], 'illinois': ['il'], 'indiana': ['in'], 'kansas': ['ks'], 
+    'kentucky': ['ky'], 'louisiana': ['la'], 'massachusetts': ['ma','mass'], 'maryland': ['md'], 'maine': ['me'], 'michigan': ['mi'], 'minnesota': ['mn','minn'], 'missouri': ['mo'], 
+    'mississippi': ['ms'], 'montana': ['mt'], 'north carolina': ['nc'], 'north dakota': ['nd'], 'nebraska': ['ne'], 'new hampshire': ['nh'], 'new jersey': ['nj'], 'new mexico': ['nm'], 
+    'nevada': ['nv'], 'new york': ['ny'], 'ohio': ['oh'], 'oklahoma': ['ok'], 'oregon': ['or'], 'pennsylvania': ['pa','penn'], 'puerto rico': ['pr'], 'rhode island': ['ri'], 
+    'south carolina': ['sc'], 'south dakota': ['sd'], 'tennessee': ['tn'], 'texas': ['tx'], 'utah': ['ut'], 'virginia': ['va'], 'vermont': ['vt'], 'washington': ['wa'], 
+    'wisconsin': ['wi'], 'west virginia': ['wv','wva'], 'wyoming': ['wy','wyo'],'state':[]}
+
+    # only checking first 500 characters of text, where the main idea of an article is usually introduced (checking the full article could be slow + introduce false negatives)
+    openingText = processText(text[:500]).lower() 
+    processed_title = processText(title)
+    title_split = processed_title.split()
+    processed_title = processed_title.lower()
+    for state in states:
+        terms = [state]
+        abvs = states[state]
+        for t in title_split:
+            # specifically checking for state abbreviations here, which are nearly always upper or mixed case (all lower implies a word or part of a string)
+            # "In" is an exception to the rule given that it could very well be used as a word and not an abbrevation
+            if t.lower() in abvs and not t.islower() and t not in ["In"]: 
+                terms.append(t.lower())
+        comparisons = generate_court_terms(terms)
+        # only checking for terms containing a full state string in the full text (first 3 elements of comparisons array) to avoid false positives
+        if any(c in processed_title or (c in openingText and i < 3) for i,c in enumerate(comparisons)):  
+            return True
+    return False
 
 # checking whether an article pertains to a foreign supreme court - True if so, False if not
 # very similar to stateCourtDetected()
-def foreignCourtDetected(source,text,title,url):
-    foreignSources = ['indiatimes','thehindu','liberianobserver','vanguardngr','allafrica','firstpost','ndtv','news18'] # sources that near exclusively (if not entirely) report on foreign supreme court news, so blocking them manually here
+def foreignCourtDetected(source,url,text,title):
+    foreignSources = ['indiatimes','thehindu','liberianobserver',"allafrica",'vanguardngr','firstpost','ndtv','news18','moneycontrol','newzimbabwe'] # sources that near exclusively (if not entirely) report on foreign supreme court news, so blocking them manually here
     if source.lower() in foreignSources or ('india' in url.lower() and 'indiana' not in url.lower()): # indian supreme court pops ups a lot, so block any source with "india" in it ("indiana" still passes)
         return True
+
     # dict in key:[] format where key is country, [] consists of the country's demonyms that could be used to refer to a foreign court (Russia Supreme Court = Russian Supreme Court, etc.)
     countries = {'afghanistan': ['afghan'], 'albania': ['albanian'], 'algeria': ['algerian'], 'andorra': ['andorran'], 'angola': ['angolan'], 'antigua and barbuda': ['antiguan', 
     'barbudan'], 'argentina': ['argentine', 'argentinean', 'argentinian'], 'armenia': ['armenian'], 'australia': ['aussie', 'australian'], 
@@ -227,6 +204,7 @@ def foreignCourtDetected(source,text,title,url):
     'tajikistan': ['tadzhik', 'tajik', 'tajikistani'], 'tanzania': ['tanzanian'], 'thailand': ['thai'], 'trinidad and tobago': ['tobagonian', 'trinibagonian', 'trinidadian'], 'togo': ['togolese'], 
     'tonga': ['tongan'], 'tunisia': ['tunisian'], 'turkey': ['turk', 'turkish'], 'turkmenistan': ['turkmen', 'turkmenistani'], 'tuvalu': ['tuvaluan'], 'uganda': ['ugandan'], 'ukraine': ['ukrainian'], 
     'uruguay': ['uruguayan'], 'uzbekistan': ['uzbek', 'uzbekistani'], 'venezuela': ['venezuelan'], 'vietnam': ['vietnamese'], 'yemen': ['yemeni', 'yemenite'], 'zambia': ['zambian'], 'zimbabwe': ['zimbabwean']}
+
     openingText = processText(text.replace('-', ' ')).lower() # replace dashes with spaces to accomodate names in 'countries' list like guinea-bissau (among others) for parsing
     processed_title = processText(title.replace('-',' ')).lower()
     title_split = processed_title.split()
@@ -244,36 +222,25 @@ def foreignCourtDetected(source,text,title,url):
         if any(c in processed_title or c in openingText for c in comparisons):
             return True
     return False
-
-# determine whether the focus of an article is about a state Supreme Court - returns True if deemed so, False otherwise
-# done by parsing the title and part of the text for giveaway terms ("[state] Supreme Court", "[state] high court", "[state] top court"...)
-def stateCourtDetected(text,title):
-    # dict in key:[] format where key is state and [] consists of common state abbreviations
-    states = {'alaska': ['ak'], 'alabama': ['al','ala'], 'arkansas': ['ar'], 'arizona': ['az','ariz'], 'california': ['ca','calif'], 'colorado': ['co'], 'connecticut': ['ct'], 'delaware': ['de'], 
-    'florida': ['fl','fla'], 'georgia': ['ga'], 'hawaii': ['hi'], 'iowa': ['ia'], 'idaho': ['id'], 'illinois': ['il'], 'indiana': ['in'], 'kansas': ['ks'], 
-    'kentucky': ['ky'], 'louisiana': ['la'], 'massachusetts': ['ma','mass'], 'maryland': ['md'], 'maine': ['me'], 'michigan': ['mi'], 'minnesota': ['mn','minn'], 'missouri': ['mo'], 
-    'mississippi': ['ms'], 'montana': ['mt'], 'north carolina': ['nc'], 'north dakota': ['nd'], 'nebraska': ['ne'], 'new hampshire': ['nh'], 'new jersey': ['nj'], 'new mexico': ['nm'], 
-    'nevada': ['nv'], 'new york': ['ny'], 'ohio': ['oh'], 'oklahoma': ['ok'], 'oregon': ['or'], 'pennsylvania': ['pa','penn'], 'puerto rico': ['pr'], 'rhode island': ['ri'], 
-    'south carolina': ['sc'], 'south dakota': ['sd'], 'tennessee': ['tn'], 'texas': ['tx'], 'utah': ['ut'], 'virginia': ['va'], 'vermont': ['vt'], 'washington': ['wa'], 
-    'wisconsin': ['wi'], 'west virginia': ['wv','wva'], 'wyoming': ['wy','wyo'],'state':[]}
-    # only checking first 500 characters of text, where the main idea of an article is usually introduced (checking the full article could be slow + introduce false negatives)
-    openingText = processText(text[:500]).lower() 
-    processed_title = processText(title)
-    title_split = processed_title.split()
-    processed_title = processed_title.lower()
-    for state in states:
-        terms = [state]
-        abvs = states[state]
-        for t in title_split:
-            # specifically checking for state abbreviations here, which are nearly always upper or mixed case (all lower implies a word or part of a string)
-            # "In" is an exception to the rule given that it could very well be used as a word and not an abbrevation
-            if t.lower() in abvs and not t.islower() and t not in ["In"]: 
-                terms.append(t.lower())
-        comparisons = generate_court_terms(terms)
-        # only checking for terms containing a full state string in the full text (first 3 elements of comparisons array) to avoid false positives
-        if any(c in processed_title or (c in openingText and i < 3) for i,c in enumerate(comparisons)):  
-            return True
-    return False
+    
+# if we can't determine relevancy with giveaways in the text or title, then determine using our text classifier
+def classify(clf,probabilities,keywords,justice_keys):
+    prob_by_class = dict(zip(clf.classes_,probabilities)) # zip probabilities to their according class
+    sorted_probs = sorted(prob_by_class.items(),key=lambda kv: kv[1],reverse=True) # sort dict into an array of tuples most to least probable by class format: [(class,probability)]
+    code = sorted_probs[0][0] # initial / highest classification
+    class_score = sorted_probs[0][1] # highest probability
+    if code == 'R': 
+        # our classification is very good but not perfect
+        # to account for false negatives, if an article is classified as relevant but certain keywords do not exist, then the classifier must be extra sure (higher probability) it is relevant before truly deeming it relevant
+        # this is called our "relevancy threshold" test
+        if not (('supreme' in keywords and 'court' in keywords) or 'scotus' in keywords) and not any(justice in keywords for justice in justice_keys): 
+            relevancy_threshold = 0.75 # numbers subject to change (still trying to find a "sweet spot" but this seems to do well)
+        else:
+            relevancy_threshold = 0.5
+        if class_score < relevancy_threshold:
+            code = sorted_probs[1][0] # if article fails threshold test, then finally classify it as the second most likely class
+            class_score = sorted_probs[1][1] 
+    return code, class_score
 
 # used for state and foreign filters
 # parameter is an array of strings (states/countries and potentially their abbreviations/demonyms) 
@@ -303,7 +270,7 @@ def main():
     db = MySQLdb.connect(host=os.environ['DB_HOST'],port=int(os.environ['DB_PORT']),user=os.environ['DB_USER'],password=os.environ['DB_PASSWORD'],db="SupremeCourtApp",use_unicode=True,charset="utf8")
     db.autocommit(True)
     c = db.cursor(MySQLdb.cursors.DictCursor)  
-    n = 5
+    n = 1
     #binary_raw = test_raw(c,n,True)
     #multi_raw = test_raw(c,n,False)
     binary_prac = test_practical(c,n,True)
